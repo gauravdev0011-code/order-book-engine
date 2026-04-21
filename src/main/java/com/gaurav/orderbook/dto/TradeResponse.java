193f5adc6a40e8a279362f3559b373a6684723be
Gaurav Dev <gauravdev0011@gmail.com>
2026-04-21 11:18:19 -0500

    feat(dto): add order request and trade response models

--- src/main/java/com/gaurav/orderbook/dto/TradeResponse.java
+++ src/main/java/com/gaurav/orderbook/dto/TradeResponse.java
@@ -1,4 +1,10 @@
 package com.gaurav.orderbook.dto;
 
+import lombok.Data;
+
+@Data
 public class TradeResponse {
+    private String tradeId;
+    private double price;
+    private int quantity;
 }
