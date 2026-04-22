80ef212a0eb5857127b2fc61cad00bbe467caf3a
Gaurav Dev <gauravdev0011@gmail.com>
2026-04-22 12:42:55 -0500

    feat(websocket): add websocket config and trade publisher

--- src/main/java/com/gaurav/orderbook/websocket/TradePublisher.java
+++ src/main/java/com/gaurav/orderbook/websocket/TradePublisher.java
@@ -1,4 +1,19 @@
 package com.gaurav.orderbook.websocket;
 
+import com.gaurav.orderbook.model.Trade;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TradePublisher {
+
+    private final SimpMessagingTemplate template;
+
+    public TradePublisher(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    public void publish(Trade trade) {
+        template.convertAndSend("/topic/trades", trade);
+    }
 }
